7161549e1c5b351d160a7b82e24f65734f93fb50
Ruslan <r.petunin@it.ecommpay.com>
2025-07-08 19:18:20 +0300

    Factory exported

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import type { PaymentMethodFactory } from 'orchestrator-pp-flow';
+
+const paymentMethodFactory: PaymentMethodFactory = async (initData, projectSettings) => {
+    return [];
+};
+
+export default paymentMethodFactory;
