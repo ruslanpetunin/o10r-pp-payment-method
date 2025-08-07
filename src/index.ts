2c7845f73457518a53d9eab1a2d6dca4702daa66
Ruslan <r.petunin@it.ecommpay.com>
2025-08-07 14:57:49 +0300

    Codestyle fix

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,11 @@ import type { Api } from 'o10r-pp-core'
 export * from 'o10r-pp-form';
 export type * from 'o10r-pp-form';
 
-const usePaymentMethodFactory = (api: Api, token: string): PaymentMethodFactory => {
+export * from './types';
+
+export default function (api: Api, token: string): PaymentMethodFactory {
   return {
     fromConfig: async (config) => makeBasePaymentMethod(config),
     fromSavedCard: async (card) => makeSavedCardPaymentMethod(api, card, token),
   }
-}
-
-export * from './types';
-
-export default usePaymentMethodFactory;
+};
