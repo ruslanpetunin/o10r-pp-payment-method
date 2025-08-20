74e8dd94eab073074be4f25c197f75fce7f750e2
Ruslan <r.petunin@it.ecommpay.com>
2025-08-20 16:53:55 +0300

    Updating core

--- src/base/index.ts
+++ src/base/index.ts
@@ -5,12 +5,13 @@ import useGenerator from './composables/useGenerator';
 
 export default function(config: PaymentMethodData): PaymentMethod {
   const { generateId } = useGenerator();
-  const paymentForm = useForm(config.fields);
+  const paymentForm = useForm(config.schema);
 
   return {
     id: generateId(),
     code: config.code,
-    icon: config.icon,
+    // icon: config.icon,
+    icon: 'https://js.stripe.com/v3/fingerprinted/img/card-ce24697297bd3c6a00fdd2fb6f760f0d.svg',
     paymentForm: paymentForm,
     getCollectedData: paymentForm.getCollectedData
   }
